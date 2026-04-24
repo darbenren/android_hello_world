c63d9ad9edc8c957457d4aecd5482a30c5b1cec3
darbenren <1052162857@qq.com>
2026-04-24 09:52:55 +0800

    MainActivity.java

--- app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
+++ app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
@@ -1,22 +1,39 @@
-package codepath.demos.helloworlddemo;
+package com.example.rotatecam;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
 
-public class HelloWorldActivity extends Activity {
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hello_world);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSurfaceView = new SurfaceView(this);
+        setContentView(mSurfaceView);
+        mSurfaceView.getHolder().addCallback(this);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_hello_world, menu);
-		return true;
-	}
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera = Camera.open();
+            // 核心功能：旋转预览画面 180 度
+            mCamera.setDisplayOrientation(180); 
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (Exception e) { e.printStackTrace(); }
+    }
 
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+        }
+    }
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {}
 }
